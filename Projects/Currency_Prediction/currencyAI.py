b55f3684a788c65bd96a40601aef615bf1d8a250
Esther Tan <tan.esther8@gmail.com>
2026-02-14 13:19:20 -0500

    k

---
+++ Projects/Currency_Prediction/currencyAI.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# find a dataset that contains the currency exchange
+
+
+# train the model using simple linear regression 
+# evaluate with cross validation set 
+# fix the high bias/variance problem if any
+
+
